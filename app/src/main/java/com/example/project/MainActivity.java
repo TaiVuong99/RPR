a6ad07213973a9c7ede658a94c714f540d4df357
TaiVuong99 <taivuongduy2@gmail.com>
2021-04-10 22:22:59 +0700

    first simple init

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView txtMoney;
+    SeekBar sbMain, sb1, sb2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
